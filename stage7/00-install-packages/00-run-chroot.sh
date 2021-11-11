c8f95edc293267a18f21a0218b8fb62c6f71ad6a
aaronhanson <aaronhanson@users.noreply.github.com>
2021-11-11 09:46:41 -0600

    tweak memory allocation

--- stage7/00-install-packages/00-run-chroot.sh
+++ stage7/00-install-packages/00-run-chroot.sh
@@ -4,3 +4,6 @@ sudo -u pi git clone https://gitlab.com/singleballplay/picam.git /home/pi/picam
 
 cd /home/pi/picam
 sudo -u pi ./pi-gen-setup
+
+# allocate more memory to gpu
+sudo raspi-config nonint do_memory_split 128
