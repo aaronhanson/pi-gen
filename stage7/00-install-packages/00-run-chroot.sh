1ab1ceaab5a1698fcd753dd0ce28e980339be81c
aaronhanson <aaronhanson@users.noreply.github.com>
2024-02-14 14:45:12 -0600

    add package for controlling kiyo cameras

--- stage7/00-install-packages/00-run-chroot.sh
+++ stage7/00-install-packages/00-run-chroot.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# package to support Razer webcams and Brio advanced features
+sudo -u pi git clone https://github.com/soyersoyer/cameractrls.git /home/pi/cameractrls
+
+# main picam project code
 sudo -u pi git clone https://gitlab.com/singleballplay/picam.git /home/pi/picam
 
 cd /home/pi/picam
