41c5aa807aa7728951ffb85f0dd64d3537b0a22f
aaronhanson <1696349+aaronhanson@users.noreply.github.com>
2024-08-03 16:09:46 -0500

    use chrony for ntp sync

--- stage7/00-install-packages/00-run-chroot.sh
+++ stage7/00-install-packages/00-run-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # package to support Razer webcams and Brio advanced features
-sudo -u pi git clone https://github.com/soyersoyer/cameractrls.git /home/pi/cameractrls
+sudo -u pi git clone --branch kiyoproultra https://github.com/soyersoyer/cameractrls.git /home/pi/cameractrls
 
 # main picam project code
 sudo -u pi git clone https://gitlab.com/singleballplay/picam.git /home/pi/picam
